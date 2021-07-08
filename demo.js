ea750b5e97e955b1c4bd7d9eeda451a8e0f90054
mawenqing1 <529837505@qq.com>
2021-07-08 10:19:08 +0800

    更新声明变量及箭头函数

--- demo.js
+++ demo.js
@@ -1,18 +1,18 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 // 点击显示隐藏赋值
-var isShow = true;
+let isShow = true;
 // 图片缩放声明
-var type = '';
+let type = '';
 // 图片透明度声明
-var transparency = '';
+let transparency = '';
 // 增加阴影声明
-var shadow = '';
+let shadow = '';
 
 // 绘制矩形
 
-var a_btn0 = document.getElementById("a-btn0");
-a_btn0.onclick = function() {
+const a_btn0 = document.getElementById("a-btn0");
+a_btn0.onclick = () => {
     if (isShow) {
         ctx.fillRect(150, 150, 400, 400);
         isShow = false;
@@ -23,32 +23,32 @@ a_btn0.onclick = function() {
 }
 
 // 更改颜色
-var btn = document.getElementById("btn");
-var input = document.getElementById("input");
-btn.addEventListener('click', function() {
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+btn.addEventListener('click', () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.rect(150, 150, 400, 400);
     ctx.fillStyle = input.value;
     ctx.fill();
 
 }, false);
-// 图片缩放
-var slider = document.getElementById("scale-range");
 
-slider.addEventListener('change', function() {
+// 图片缩放
+const slider = document.getElementById("scale-range");
+slider.addEventListener('change', () => {
     ctx.clearRect(0, 0, 700, 700);
     ctx.save();
     switch (type) {
         case 'img':
-            var img = new Image();
-            var file = document.getElementById("file").files[0];
+            let img = new Image();
+            let file = document.getElementById("file").files[0];
             // 采用fileReader构造器将上传图片转化为64位编码
-            var reader = new FileReader();
+            let reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onload = function() {
+            reader.onload = () => {
                 var data = reader.result;
                 img.src = data;
-                img.onload = function() {
+                img.onload = () => {
                     ctx.drawImage(img, 150, 150, img.width * slider.value, img.height * slider.value);
                 }
             };
@@ -78,8 +78,8 @@ slider.addEventListener('change', function() {
 })
 
 // 图层叠加
-var btn1 = document.getElementById("btn1");
-btn1.addEventListener('click', function() {
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener('click', () => {
     console.log(111);
     ctx.clearRect(200, 200, 200, 200);
     ctx.rect(200, 200, 200, 200);
@@ -89,9 +89,9 @@ btn1.addEventListener('click', function() {
 
 }, false);
 // 更改图层颜色
-var btn2 = document.getElementById("btn2");
-var input1 = document.getElementById("input1");
-btn2.addEventListener('click', function() {
+const btn2 = document.getElementById("btn2");
+const input1 = document.getElementById("input1");
+btn2.addEventListener('click', () => {
     ctx.clearRect(0, 0, 700, 700);
     ctx.rect(200, 200, 200, 200);
     ctx.fillStyle = input1.value;
@@ -99,8 +99,8 @@ btn2.addEventListener('click', function() {
 }, false)
 
 // 绘制三角形
-var a_btn1 = document.getElementById("a-btn1");
-a_btn1.onclick = function() {
+const a_btn1 = document.getElementById("a-btn1");
+a_btn1.onclick = () => {
     type = 'triangle';
     transparency = 'triangle';
     shadow = 'triangle';
@@ -119,8 +119,8 @@ a_btn1.onclick = function() {
 }
 
 // 绘制圆形
-var a_btn2 = document.getElementById("a-btn2");
-a_btn2.onclick = function() {
+const a_btn2 = document.getElementById("a-btn2");
+a_btn2.onclick = () => {
     type = 'circle';
     transparency = 'circle';
     shadow = 'circle';
@@ -138,32 +138,32 @@ a_btn2.onclick = function() {
 }
 
 // 选择图片
-var a_btn3 = document.getElementById("a-btn3");
-var a_btn6 = document.getElementById("a-btn6");
-a_btn3.addEventListener('click', function() {
+const a_btn3 = document.getElementById("a-btn3");
+const a_btn6 = document.getElementById("a-btn6");
+a_btn3.addEventListener('click', () => {
     type = 'img';
     transparency = 'img';
     shadow = 'img';
-    var img = new Image();
-    var file = document.getElementById("file").files[0];
+    let img = new Image();
+    let file = document.getElementById("file").files[0];
     // 采用fileReader构造器将上传图片转化为64位编码
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function() {
         var data = reader.result;
         img.src = data;
-        img.onload = function() {
+        img.onload = () => {
             // 添加拖拽效果
-            a_btn6.onclick = function() {
-                var x = 0;
-                var y = 0;
-                canvas.onclick = function(e) {
+            a_btn6.onclick = () => {
+                let x = 0;
+                let y = 0;
+                canvas.onclick = (e) => {
                     x = e.offsetX - img.width / 2;
                     y = e.offsetY - img.height / 2;
-                    console.log(x, y);
+                    // console.log(x, y);
                 };
-                setInterval(function() {
-                    ctx.clearRect(0, 0, 700, 700);
+                setInterval(() => {
+                    ctx.clearRect(0, 0, canvas.width, canvas.height);
                     ctx.drawImage(img, x, y);
                 }, 20);
             }
@@ -173,8 +173,8 @@ a_btn3.addEventListener('click', function() {
 })
 
 // 平铺
-var a_btn4 = document.getElementById("a-btn4");
-a_btn4.addEventListener('click', function() {
+const a_btn4 = document.getElementById("a-btn4");
+a_btn4.addEventListener('click', () => {
     var img = new Image();
     var file = document.getElementById("file").files[0];
     var reader = new FileReader();
@@ -182,7 +182,7 @@ a_btn4.addEventListener('click', function() {
     reader.onload = function() {
         var data = reader.result;
         img.src = data;
-        img.onload = function() {
+        img.onload = () => {
             var pattern = ctx.createPattern(img, 'repeat');
             ctx.fillStyle = pattern;
             ctx.fillRect(150, 150, 400, 400);
@@ -191,16 +191,16 @@ a_btn4.addEventListener('click', function() {
 })
 
 // 旋转
-var a_btn5 = document.getElementById("a-btn5");
-a_btn5.addEventListener('click', function() {
-    var img = new Image();
-    var file = document.getElementById("file").files[0];
-    var reader = new FileReader();
+const a_btn5 = document.getElementById("a-btn5");
+a_btn5.addEventListener('click', () => {
+    let img = new Image();
+    let file = document.getElementById("file").files[0];
+    let reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = function() {
-        var data = reader.result;
+    reader.onload = () => {
+        let data = reader.result;
         img.src = data;
-        img.onload = function() {
+        img.onload = () => {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.translate(canvas.width / 2, canvas.height / 2);
             ctx.rotate(Math.PI / 6);
@@ -212,22 +212,22 @@ a_btn5.addEventListener('click', function() {
 
 // 透明度
 
-var tp = document.getElementById("tp-range");
-tp.addEventListener('change', function() {
-    var num = parseFloat(tp.value);
+const tp = document.getElementById("tp-range");
+tp.addEventListener('change', () => {
+    let num = parseFloat(tp.value);
     ctx.clearRect(0, 0, 700, 700);
     ctx.save();
     switch (transparency) {
         case 'img':
-            var img = new Image();
-            var file = document.getElementById("file").files[0];
+            let img = new Image();
+            let file = document.getElementById("file").files[0];
             // 采用fileReader构造器将上传图片转化为64位编码
-            var reader = new FileReader();
+            let reader = new FileReader();
             reader.readAsDataURL(file);
-            reader.onload = function() {
+            reader.onload = () => {
                 var data = reader.result;
                 img.src = data;
-                img.onload = function() {
+                img.onload = () => {
                     ctx.globalAlpha = num;
                     ctx.drawImage(img, 150, 150, img.width * slider.value, img.height * slider.value);
                 }
@@ -258,28 +258,28 @@ tp.addEventListener('change', function() {
 })
 
 // 增加图片阴影
-var a_btn7 = document.getElementById("a-btn7");
-var shad = function() {
+const a_btn7 = document.getElementById("a-btn7");
+const shad = () => {
     ctx.shadowOffsetX = 15;
     ctx.shadowOffsetY = 15;
     ctx.shadowBlur = 2;
     ctx.shadowColor = "rgba(0, 0, 0, 0.5)";
 }
-a_btn7.addEventListener('click', function() {
+a_btn7.addEventListener('click', () => {
     if (isShow) {
         // ctx.save();
         isShow = false;
         switch (shadow) {
             case 'img':
-                var img = new Image();
-                var file = document.getElementById("file").files[0];
+                let img = new Image();
+                let file = document.getElementById("file").files[0];
                 // 采用fileReader构造器将上传图片转化为64位编码
-                var reader = new FileReader();
+                let reader = new FileReader();
                 reader.readAsDataURL(file);
-                reader.onload = function() {
+                reader.onload = () => {
                     var data = reader.result;
                     img.src = data;
-                    img.onload = function() {
+                    img.onload = () => {
                         shad();
                         ctx.globalAlpha = num;
                         ctx.drawImage(img, 150, 150, img.width * slider.value, img.height * slider.value);
